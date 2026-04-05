8ef05ec9c8c19f6cee050e7ddb8a99691fe04994
Jitesh Zope <jiteshzope26031995@gmail.com>
2026-04-05 14:01:10 +0200

    pipes

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -9,6 +9,18 @@ export class HomePageComponent implements OnInit {
 
   name: string = "John Doe";
 
+  today: Date = new Date(); // create a Date object
+  // a newly created date object contains the current date and time
+  // we can change the date and time by passing parameters to the Date constructor
+  // for example, to create a date object for January 1, 2020, we can do:
+  anotherDate = new Date(2020, 0, 1); // month is 0-indexed
+
+  myObj = {
+    name: "Alice",
+    age: 30,
+    city: "New York"
+  }
+
   constructor() { }
 
   ngOnInit(): void {
