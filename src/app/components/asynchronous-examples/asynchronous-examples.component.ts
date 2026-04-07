5450fec9059ea340a68e3ea6f0787e3f0444b731
Jitesh Zope <jiteshzope26031995@gmail.com>
2026-04-07 16:06:04 +0200

    rxjs operators

--- src/app/components/asynchronous-examples/asynchronous-examples.component.ts
+++ src/app/components/asynchronous-examples/asynchronous-examples.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { error } from 'console';
-import { Observable } from 'rxjs';
+import { filter, map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-asynchronous-examples',
@@ -14,27 +13,56 @@ export class AsynchronousExamplesComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    this.myObservable = new Observable((observer) => {
+    // this.myObservable = new Observable((observer) => {
+    //   let i = 1;
+    //   setInterval(() => {
+    //     observer.next(i + ' mayuiri');
+    //     if(i === 5) {
+    //       observer.complete();
+    //     }
+    //     if(i === 4){
+    //       observer.error('An error occurred');
+    //     }
+    //     i++;
+    //   }, 1000);
+
+    // });
+
+    // this.myObservable.subscribe({
+    //   // data or next callback.... this will be called whenever observer.next() is called in observable
+    //   next: (data) => {console.log(data)},
+    //   // error callback ... once error is emitted using observer.error(), observable will stop emitting data and complete callback will not be called
+    //   error: (error) => {console.error(error)},
+    //   // complete callback ... this will be called once observer.complete() is called and no more data will be emitted
+    //   complete: () => {console.log('Observable completed')}
+    // });
+
+    const observable1 = new Observable((observer) => {
       let i = 1;
       setInterval(() => {
-        observer.next(i + ' mayuiri');
+        observer.next(i);
         if(i === 5) {
           observer.complete();
         }
-        if(i === 4){
-          observer.error('An error occurred');
-        }
         i++;
       }, 1000);
-
     });
 
-    this.myObservable.subscribe({
-      // data or next callback.... this will be called whenever observer.next() is called in observable
+    const newObservable = observable1.pipe(
+      filter(
+        (data : any) => data % 2 === 0
+      ),
+      map(
+        (data : any) => data * 2
+      ),
+      map(
+        (data : any) => data + ' mayuiri'
+      )
+    )
+
+    newObservable.subscribe({
       next: (data) => {console.log(data)},
-      // error callback ... once error is emitted using observer.error(), observable will stop emitting data and complete callback will not be called
       error: (error) => {console.error(error)},
-      // complete callback ... this will be called once observer.complete() is called and no more data will be emitted
       complete: () => {console.log('Observable completed')}
     });
 
